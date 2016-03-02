2c94db2bf650e4f58924ff19f6da70ef30b2c7e4
Matt Stjernholm <thew88@gmail.com>
2016-03-02 10:56:06 -0700

    vote count is displayed on page

--- public/client.js
+++ public/client.js
@@ -1,6 +1,7 @@
 var socket = io();
 
 var connectionCount = document.getElementById('connection-count');
+var voteCount = document.getElementById('vote-count');
 var statusMessage = document.getElementById('status-message');
 var buttons = document.querySelectorAll('#choices button');
 
@@ -11,7 +12,11 @@ for (var i = 0; i < buttons.length; i++) {
 }
 
 socket.on('voteCount', function (votes) {
-  console.log(votes);
+  results = ""
+  for (var vote in votes) {
+    results = results + vote + ": " + votes[vote] + "  "
+  }
+  voteCount.innerText = results
 });
 
 socket.on('usersConnected', function (count) {
