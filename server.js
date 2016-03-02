f809bd8898e6f7a36c4005a6207a22354aa3e797
Matt Stjernholm <thew88@gmail.com>
2016-03-02 09:47:09 -0700

    counts disconnected users

---
+++ server.js
@@ -0,0 +1,28 @@
+const http = require('http');
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 3000;
+const server = http.createServer(app)
+                 server.listen(port, function () {
+                    console.log('Listening on port ' + port + '.');
+                  });
+
+const socketIo = require('socket.io');
+const io = socketIo(server);
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res){
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+io.on('connection', function (socket) {
+  console.log('A user has connected.', io.engine.clientsCount);
+
+  socket.on('disconnect', function () {
+    console.log('A user has disconnected.', io.engine.clientsCount);
+  });
+});
+
+module.exports = server;
